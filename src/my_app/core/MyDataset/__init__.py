ac7e32dbdac0796189cdb7ca95a715106dff9878
riku213 <kotatorameme3@gmail.com>
2025-08-30 17:09:55 +0900

    サイズ別キャッシュ生成を行うデータベース型作成

--- src/my_app/core/MyDataset/__init__.py
+++ src/my_app/core/MyDataset/__init__.py
@@ -1,3 +1,4 @@
 from .MyDataset_v0 import PreTrainDataset, create_optimized_dataloader
 from .MyDataset_v2 import PreTrainDataset_v2
-__all__ = ['PreTrainDataset', 'create_optimized_dataloader', 'PreTrainDataset_v2'] 
+from .MyDataset_v3 import PreTrainDataset_v3
+__all__ = ['PreTrainDataset', 'create_optimized_dataloader', 'PreTrainDataset_v2', 'PreTrainDataset_v3'] 
--- src/my_app/core/__init__.py
+++ src/my_app/core/__init__.py
@@ -1,7 +1,7 @@
 # 各サブパッケージから主要なクラスをインポート
-from .MyDataset import PreTrainDataset, create_optimized_dataloader
+from .MyDataset import PreTrainDataset, PreTrainDataset_v3, create_optimized_dataloader
 from .MyNetworks import UNet
 from .MyNetworks import DeepUNet
-__all__ = ['UNet', 'DeepUNet', 'PreTrainDataset']
+__all__ = ['UNet', 'DeepUNet', 'PreTrainDataset', 'PreTrainDataset_v3']
 # module.py から主要な関数をインポート
 # from .module import run_process # ← module.pyにrun_process関数がある場合
